4c5c0bf69a57a3a3ad0c7b4d9149eb82668428ba
Ankit Kataria <ankitkataria66@gmail.com>
2017-10-11 13:03:34 +0530

    created an option for input

--- src/hello/Grade.java
+++ src/hello/Grade.java
@@ -1,8 +1,11 @@
 package hello;
+import java.util.Scanner;
 
 public class Grade {
     public static void main(String[] args) {
-        int g = -100;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter grade");
+        int g=sc.nextInt();
         if(g<0 || g>100)
             System.out.println("Invalid input");
         else if (g>=85 && g<=100)
